298ac8d94fd7af6479fff4ed65c92f2360ca5af6
gurunathkamale <ramgurunath7@gmail.com>
2025-12-27 19:29:58 +0530

    changes done in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,11 @@
-// import { auth } from "../utilitis/auth"
-
-
 const Home = () => {
-  // const handleClick = ()=>{
-  //   auth.isAuthenticated = 
-  // }
   return (
     <div>
       <h2>home page</h2>
       <p>Only Login Users can access</p>
-      <div className="flex justify-center">
-
-{/* <button onClick={handleClick} className="text-center text-red-500 w-16 h-8 bg-green-300">Click</button> */}
-      </div>
+    
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
